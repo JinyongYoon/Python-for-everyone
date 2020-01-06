de407c9c9f17053886c02eb602f5dc279fc31bbc
YoonJinYong <balloon1999@naver.com>
2020-01-06 20:51:33 +0900

    Python3_complete

---
+++ py4e3 - Access Web Data/exercise/exercise ch11~12.py
@@ -0,0 +1,58 @@
+# # Exercise (Regular Expressions)
+# import re
+# handle = open("/Users/jinyong/py4e/actual.txt")
+# total = 0
+# x = list()
+# for line in handle:
+#     y = re.findall('[0-9]+', line)
+#     if len(y) == 0:
+#          continue
+#     for num in range(0, len(y)):
+#         total = total + int(y[num])
+
+# print(total)
+
+
+# # Exercise of HTTP Request 
+# import socket
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect(('data.pr4e.org', 80))
+# cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+# mysock.send(cmd)
+
+# while True:
+#     data = mysock.recv(512)
+#     if len(data) < 1:
+#         break
+#     print(data.decode())
+
+# mysock.close()
+
+
+# # Exercise of urllib
+# import urllib.request, urllib.parse, urllib.error
+
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+# counts = dict()
+# for line in fhand:
+#     words = line.decode().split()
+#     for word in words:
+#         counts[word] = counts.get(word, 0) + 1
+
+# print(counts)
+
+
+# # Exercise of BeautifulSoup
+# import urllib.request, urllib.parse, urllib.error
+# from bs4 import BeautifulSoup
+
+# url = input('Enter - ')
+# html = urllib.request.urlopen(url).read()
+# soup = BeautifulSoup(html, 'html.parser')
+
+# # Retrieve all of the anchor tags
+# tags = soup('a')
+# for tag in tags:
+#     print(tag.get('href', None))
+
